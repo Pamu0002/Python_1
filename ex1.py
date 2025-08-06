24511e09ced19352eb08ad369db4e2db1082bc7c
Pamudi <pamukavinshaa@gmail.com>
2025-08-06 13:07:10 +0530

    First code

---
+++ ex1.py
@@ -0,0 +1,42 @@
+# print("welcome to python")
+# print (7+3)
+# print(type(6.66))
+# print(10%2==0)
+# print("AI" + "CHATBOT")
+
+
+# i=10
+# if (i<15):
+#     print("i is smaller than 15")
+#     print("i m in if block")
+
+# else:
+#     print("i is greater than 15")
+#     print("i'm in else block")
+#     print("i'm not in if and not in else block")
+
+
+
+# i=20
+# if (i ==10):
+#     print("i is 10")
+# elif(i==15):
+#      print("i is 10")
+# elif("i== 20"):
+#       print("i is 20")
+# else:
+#       print("i is not present")
+
+i=1
+while i< 6:
+    print(i)
+    i+=1
+
+fruits= ["apple" ,"banana" ,"cherry"]
+for x in fruits:
+    print(x)
+
+
+marks=int(input("Enter your marks: "))
+print (type(marks)) 
+
